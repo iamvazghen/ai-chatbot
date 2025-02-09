e580677ef261b35070e33d98b5ca77cb0569cd25
iamvazghen <iamvazghen@gmail.com>
2025-02-09 22:04:27 +0100

    Update models.ts

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -1,9 +1,5 @@
 import { openai } from '@ai-sdk/openai';
 import { fireworks } from '@ai-sdk/fireworks';
-import { mistral} from '@ai-sdk/mistral';
-import { anthropic } from '@ai-sdk/anthropic';
-import { cohere } from '@ai-sdk/cohere';
-import { google } from '@ai-sdk/google'
 import {
   customProvider,
   extractReasoningMiddleware,
@@ -22,12 +18,6 @@ export const myProvider = customProvider({
     }),
     'title-model': openai('gpt-4-turbo'),
     'block-model': openai('gpt-4o-mini'),
-    'gemini-2': google('gemini-2.0-flash-001'),
-    'cohere-command': cohere('command'),
-    'claude-sonnet': anthropic('claude-3-sonnet-20240229'),
-    'mistral-large': mistral('mistral-large-latest'),
-    'deepseek-v3-model': fireworks('accounts/fireworks/models/deepseek-v3'),
-    'llama-v3p1-model': fireworks('accounts/fireworks/models/llama-v3p1-405b-instruct'),
   },
   imageModels: {
     'small-model': openai.image('dall-e-2'),
@@ -54,37 +44,7 @@ export const chatModels: Array<ChatModel> = [
   },
   {
     id: 'chat-model-reasoning',
-    name: 'DeepSeek R1',
+    name: 'Reasoning model',
     description: 'Uses advanced reasoning',
-  },
-  {
-    id: 'deepseek-v3-model',
-    name: 'DeepSeek V3',
-    description: 'Latest DeepSeek model',
-  },
-  {
-    id: 'llama-v3p1-model',
-    name: 'Llama 3.1 405B',
-    description: 'Llama model with most parameters',
-  },
-  {
-    id: 'mistral-large',
-    name: 'Mistral Large',
-    description: 'Latest model of Mistral',
-  },
-  {
-    id: 'cohere-command',
-    name: 'Cohere Command',
-    description: 'Advanced coding model',
-  },
-  {
-    id: 'gemini-2',
-    name: 'Gemini 2 Flash',
-    description: 'Model with most context',
-  },
-  {
-    id: 'claude-sonnet',
-    name: 'Claude Sonnet 3.5',
-    description: 'Advanced Anthropic model',
   },
 ];
