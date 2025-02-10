d342b242f4fb97fb9a8d0bbee21a984bb2f34039
iamvazghen <iamvazghen@gmail.com>
2025-02-10 12:41:29 +0100

    Create logo.jsx

---
+++ logo.jsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <img 
+      src="/public/images/logo.png" 
+      alt="Company Logo"
+      className="logo" 
+      width={32} 
+      height={32}
+    />
+  )
+}
